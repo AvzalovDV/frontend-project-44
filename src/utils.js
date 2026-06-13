aa38d31bb6045ed6cdeff35b918c32b9994616f0
AvzalovDV <danil.himster1@gmail.com>
2026-06-13 22:01:34 +0300

    complete2

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,2 @@
 // NOSONAR: безопасно для выбора оператора в учебном проекте
-export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min
+export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min // NOSONAR
